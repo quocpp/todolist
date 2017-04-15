c9c1a4a53f10464ea0c66fed185350b6f7f95617
quoc <quoc@quoc.com>
2017-04-15 11:57:54 +0700

    upgraded

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,35 @@
 class TasksController < ApplicationController
     def new
         @task = Task.new
+        render :show_form
     end
+
+    def save_task
+        if @task.save
+	        @task = Task.all
+            render :hide_form
+        else
+            render :show_form
+        end
+    end
+
     def create
         @task = Task.create(task_params)
-	@task = Task.all
+        save_task
     end
     def destroy
-	@task = Task.find(params[:id])
-	@task.destroy
-	@task = Task.all
+	    @task = Task.find(params[:id])
+	    @task.destroy
+	    @task = Task.all
+    end
+    def edit
+        @task = Task.find(params[:id])
+        render :show_form
+    end
+    def update
+        @task = Task.find(params[:id])
+        @task.update_attributes(task_params)
+        save_task
     end
     private
     def task_params
